408048d65d8e4cd325303e345ff1b9866fa9cfe4
a2.dunin <a2.dunin@itransition.com>
2019-07-03 19:02:08 +0300

    fix extensions for string

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -2,9 +2,18 @@ package ru.skillbranch.devintensive.extensions
 
 
 fun String.truncate(countTruncate: Int = 16): String {
-    return "${this.substring(0, countTruncate).trim()}..."
+    return if (this.trim().length > countTruncate)
+        "${this.substring(0, countTruncate).trim()}..."
+    else
+        this.trim()
+
+
 }
 
 fun String.stripHtml(): String {
-    return this.replace("<[^>]*>".toRegex(), "").replace("\\s+".toRegex(), " ").trim()
+    return this
+        .replace("<[^>]*>".toRegex(), "")
+        .replace("&(.*?);".toRegex(), "")
+        .replace("\\s+".toRegex(), " ")
+        .trim()
 }
